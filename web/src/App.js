ef77451013dd5c771f8ac753c474525491f81a37
Luciano Coelho <coelho.luciano@icloud.com>
2022-07-25 20:42:45 -0300

    web

---
+++ web/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
+
+// Componente: (App e um componente = function)     - Bloco isolado de HTML, CSS e JS o qual nao interfere no restante da aplicacao
+// Estado: (informacao que o componente manupula)   - Informacoes mantidas pelo componente (Lembrar: imutabilidade)
+// Propriedade: (Sempre que usar HTML = atributos)  - Informacoes que um componente PAI passa para o componente FILHO
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
