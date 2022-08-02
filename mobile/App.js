f4376a30913c667e89ca559dc14a496d7a795ba1
Luciano Coelho <coelho.luciano@icloud.com>
2022-08-02 19:34:31 -0300

    mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,29 +1,16 @@
+import React from 'react';
 //import { StatusBar } from 'expo-status-bar';
-//import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
-//export default function App() {
-//  return (
-//    <View style={styles.container}>
-//      <Text>Open up App.js to start working on your app!</Text>
-//      <StatusBar style="auto" />
-//    </View>
-//  );
-//}
-
-//const styles = StyleSheet.create({
-//  container: {
-//    flex: 1,
-//    backgroundColor: '#fff',
-//    alignItems: 'center',
-//    justifyContent: 'center',
-//  },
-//});
-
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+      <Routes />
+    </>
   );
 }
+
+
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
 
 function Main() {
-    return <View />
+    return (
+        <View style={styles.container}>
+          <MapView style={styles.map} />
+        </View>
+      );
 }
 
 export default Main;
+
