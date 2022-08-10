5707ccb9518f7e9bea0dc1ad4fc23a3424d27227
Luciano Coelho <coelho.luciano@icloud.com>
2022-08-09 21:54:35 -0300

    mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-//import { StatusBar } from 'expo-status-bar';
-import { StatusBar } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
 
 import Routes from './src/routes';
 
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
+
 export default function App() {
   return (
-    <>
+    <> 
       <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
       <Routes />
     </>
