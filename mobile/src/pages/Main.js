f7348c7be4979d19514a73e8a2577ecd1adca610
Luciano Coelho <coelho.luciano@icloud.com>
2022-08-09 21:07:28 -0300

    mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
-    return (
-        <View style={styles.container}>
-          <MapView style={styles.map} />
-        </View>
-      );
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return <MapView initialRegion={currentRegion} style={styles.map} />
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+})
 
 export default Main;
 
