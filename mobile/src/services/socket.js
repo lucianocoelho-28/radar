0389b355a5f7274462edf9fcf76ca3b60c98046d
Luciano Coelho <coelho.luciano@icloud.com>
2022-08-09 23:20:57 -0300

    mobile

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,6 +1,8 @@
 import socketio from 'socket.io-client';
 
-const socket = socketio('http://192.168.1.5:3333', {
+//emulador do android usar http://10.0.2.2:3333
+//para emulador usar http://localhost:3333
+const socket = socketio('http://172.16.254.135:3333', {
   autoConnect: false,
 });
 
